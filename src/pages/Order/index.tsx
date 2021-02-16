15f6ff22673294577964220a048402e732ab20fb
Moisés Escurra <moisesmagno@gmail.com>
2021-02-16 02:42:28 -0300

    Terminado parcialmente a página de pedido.

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { FaShoppingBasket } from 'icons-react/fa';
 
 import Nav from '../../components/Header';
+
+import { Section, Header } from './styles';
 
 const Order: React.FC = () => (
     <>
         <Nav />
-        <h1>order</h1>
+        <Section>
+            <header>
+                <form>
+                    <input type="text" placeholder="Código ou nome do produto"/>
+                    <button>Buscar</button>
+                </form>
+                <div>
+                    <div>
+                        <span>5</span>
+                        <FaShoppingBasket />
+                    </div>
+                    <button>Ver pedido</button>
+                </div>
+            </header>
+        </Section>
     </>
 )
 
+//https://react-icons.github.io/react-icons/icons?name=fa
 
 export default Order;
